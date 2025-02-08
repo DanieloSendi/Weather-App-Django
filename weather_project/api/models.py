d9c678993d1ef258297c37279679fb826befe7b2
DanieloSendi <danielosedlak@gmail.com>
2025-02-08 18:40:09 +0100

    Add City model and registered it in admin panel

--- weather_project/api/models.py
+++ weather_project/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+    city = models.CharField(max_length=100)
+    country_code = models.CharField(max_length=50, blank=True, null=True)
+    temperature = models.CharField(max_length=10, blank=True, null=True)
+    pressure = models.CharField(max_length=10, blank=True, null=True)
+    humidity = models.CharField(max_length=10, blank=True, null=True)
+    main = models.CharField(max_length=50, blank=True, null=True)
+    icon = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.city

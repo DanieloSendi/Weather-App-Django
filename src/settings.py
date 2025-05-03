581675d4f90be028de582503747cdbd0a9c5ce17
DanieloSendi <danielosedlak@gmail.com>
2025-05-03 23:56:27 +0200

    Updated settings.py

--- src/settings.py
+++ src/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    'api.middleware.LoggingMiddleware',
 ]
 
 ROOT_URLCONF = 'src.urls'
